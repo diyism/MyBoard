29af560ac6d9bd638b775957028fc8b19aa8389d
Malcolm Ke Win <kexianbin@diyism.com>
2018-04-12 19:59:42 +0800

    Update myboard.py

--- myboard.py
+++ myboard.py
@@ -1,8 +1,20 @@
 from Xlib.display import Display
 from Xlib import X, XK
-from Xlib.protocol.event import KeyPress, KeyRelease, ButtonPress, ButtonRelease
+from Xlib.protocol.event import KeyPress, KeyRelease, ButtonPress, ButtonRelease, ClientMessage
 from Xlib.ext.xtest import fake_input
 import time
+import os
+import socket
+from collections import namedtuple
+import json
+import webview
+import thread
+
+from gi.repository import GdkX11
+
+from pprint import pprint
+
+from gi.repository import xlib
 
 disp=Display()
 screen=disp.screen()
@@ -12,13 +24,14 @@ MOUSE_STEP_SLOW=screen.width_in_pixels/100
 is_on=0
 is_ready_switchall=0
 key_delay_shift=''
+_NET_WM_STATE_REMOVE=0
+_NET_WM_STATE_ADD=1
+_NET_WM_STATE_TOGGLE=2
 
 #X.Mod1Mask alt  X.ShiftMask   X.AnyModifier X.ControlMask
 #q, F1, Left
-keys=[{'key':'Shift_L', 'mod':X.ShiftMask},
-      {'key':'Shift_R', 'mod':X.ShiftMask},
-      {'key':'Shift_L', 'mod':X.NONE},
-      {'key':'Shift_R', 'mod':X.NONE},
+keys=[{'key':'Control_L', 'mod':X.ControlMask},
+      {'key':'Control_R', 'mod':X.ControlMask},
 
       {'key':'l', 'mod':X.NONE},
       {'key':'j', 'mod':X.NONE},
@@ -58,6 +71,7 @@ keys=[{'key':'Shift_L', 'mod':X.ShiftMask},
       {'key':'c', 'mod':X.NONE},
       {'key':'v', 'mod':X.NONE},
       {'key':'o', 'mod':X.NONE},
+      {'key':'m', 'mod':X.NONE},
       {'key':'w', 'mod':X.NONE},
       {'key':'/', 'mod':X.NONE},
       {'key':'.', 'mod':X.NONE},
@@ -67,8 +81,6 @@ keys=[{'key':'Shift_L', 'mod':X.ShiftMask},
       {'key':'h', 'mod':X.ShiftMask}
      ]
 keys_mapped=[{'key':'SwitchAll', 'mod':X.NONE, 'act':0},
-             {'key':'SwitchAll', 'mod':X.NONE, 'act':0},
-             {'key':'SwitchAll', 'mod':X.NONE, 'act':0},
              {'key':'SwitchAll', 'mod':X.NONE, 'act':0},
 
              {'key':'MoveRight', 'mod':X.NONE, 'act':MOUSE_STEP},
@@ -109,6 +121,7 @@ keys_mapped=[{'key':'SwitchAll', 'mod':X.NONE, 'act':0},
              {'key':'c', 'mod':X.ControlMask, 'act':2},
              {'key':'v', 'mod':X.ControlMask, 'act':2},
              {'key':'n', 'mod':X.ControlMask, 'act':2},
+             {'key':'d', 'mod':X.ControlMask|X.Mod1Mask, 'act':2},
              {'key':'F4', 'mod':X.Mod1Mask, 'act':2},
              {'key':'z', 'mod':X.ControlMask, 'act':2},
              {'key':'a', 'mod':X.ControlMask, 'act':2},
@@ -117,13 +130,98 @@ keys_mapped=[{'key':'SwitchAll', 'mod':X.NONE, 'act':0},
              {'key':'End', 'mod':X.ControlMask, 'act':2},
              {'key':'End', 'mod':X.ShiftMask, 'act':2}
             ]
+pinyin_keys=[#1-9,0
+             {'key':'1', 'mod':X.NONE},
+             {'key':'2', 'mod':X.NONE},
+             {'key':'3', 'mod':X.NONE},
+             {'key':'4', 'mod':X.NONE},
+             {'key':'5', 'mod':X.NONE},
+             {'key':'6', 'mod':X.NONE},
+             {'key':'7', 'mod':X.NONE},
+             {'key':'8', 'mod':X.NONE},
+             {'key':'9', 'mod':X.NONE},
+             {'key':'0', 'mod':X.NONE},
+             #a..z
+             {'key':'a', 'mod':X.NONE},
+             {'key':'b', 'mod':X.NONE},
+             {'key':'c', 'mod':X.NONE},
+             {'key':'d', 'mod':X.NONE},
+             {'key':'e', 'mod':X.NONE},
+             {'key':'f', 'mod':X.NONE},
+             {'key':'g', 'mod':X.NONE},
+             {'key':'h', 'mod':X.NONE},
+             {'key':'i', 'mod':X.NONE},
+             {'key':'j', 'mod':X.NONE},
+             {'key':'k', 'mod':X.NONE},
+             {'key':'l', 'mod':X.NONE},
+             {'key':'m', 'mod':X.NONE},
+             {'key':'n', 'mod':X.NONE},
+             {'key':'o', 'mod':X.NONE},
+             {'key':'p', 'mod':X.NONE},
+             {'key':'q', 'mod':X.NONE},
+             {'key':'r', 'mod':X.NONE},
+             {'key':'s', 'mod':X.NONE},
+             {'key':'t', 'mod':X.NONE},
+             {'key':'u', 'mod':X.NONE},
+             {'key':'v', 'mod':X.NONE},
+             {'key':'w', 'mod':X.NONE},
+             {'key':'x', 'mod':X.NONE},
+             {'key':'y', 'mod':X.NONE},
+             {'key':'z', 'mod':X.NONE},
+             #Backspace
+             {'key':'\b', 'mod':X.NONE},
+             #!,@,#,$,%,^,&,*,(,), and 11 double-sign keys:=-[]\/,.;'`
+             {'key':'1', 'mod':X.ShiftMask},
+             {'key':'2', 'mod':X.ShiftMask},
+             {'key':'3', 'mod':X.ShiftMask},
+             {'key':'4', 'mod':X.ShiftMask},
+             {'key':'5', 'mod':X.ShiftMask},
+             {'key':'6', 'mod':X.ShiftMask},
+             {'key':'7', 'mod':X.ShiftMask},
+             {'key':'8', 'mod':X.ShiftMask},
+             {'key':'9', 'mod':X.ShiftMask},
+             {'key':'0', 'mod':X.ShiftMask},
+             {'key':'=', 'mod':X.NONE},
+             {'key':'-', 'mod':X.NONE},
+             {'key':'[', 'mod':X.NONE},
+             {'key':']', 'mod':X.NONE},
+             {'key':'\\', 'mod':X.NONE},
+             {'key':'/', 'mod':X.NONE},
+             {'key':',', 'mod':X.NONE},
+             {'key':'.', 'mod':X.NONE},
+             {'key':';', 'mod':X.NONE},
+             {'key':'\'', 'mod':X.NONE},
+             {'key':'`', 'mod':X.NONE},
+             {'key':'=', 'mod':X.ShiftMask},
+             {'key':'-', 'mod':X.ShiftMask},
+             {'key':'[', 'mod':X.ShiftMask},
+             {'key':']', 'mod':X.ShiftMask},
+             {'key':'\\', 'mod':X.ShiftMask},
+             {'key':'/', 'mod':X.ShiftMask},
+             {'key':',', 'mod':X.ShiftMask},
+             {'key':'.', 'mod':X.ShiftMask},
+             {'key':';', 'mod':X.ShiftMask},
+             {'key':'\'', 'mod':X.ShiftMask},
+             {'key':'`', 'mod':X.ShiftMask},
+             #SPACE,TAB,RETURN,ESCAPE,APPS,END,HOME,UP,DOWN,LEFT,RIGHT,LWIN,RWIN,DELETE,MOD_ALT, MOD_CONTROL
+             {'key':' ', 'mod':X.NONE},
+             {'key':'\t', 'mod':X.NONE},
+             {'key':'\n', 'mod':X.NONE},
+             {'key':'\e', 'mod':X.NONE},
+             {'key':'Menu', 'mod':X.NONE},
+             {'key':'End', 'mod':X.NONE},
+             {'key':'Home', 'mod':X.NONE},
+             {'key':'Up', 'mod':X.NONE},
+             {'key':'Down', 'mod':X.NONE},
+             {'key':'Left', 'mod':X.NONE},
+             {'key':'Right', 'mod':X.NONE},
+             {'key':'Super_L', 'mod':X.NONE},
+             {'key':'Super_R', 'mod':X.NONE},
+             {'key':'Delete', 'mod':X.NONE}
+            ]
 
 def string_to_keycode(key):
-    codes_mod=[50,62,64,108,37,105]
-    names_mod=['Shift_L', 'Shift_R', 'Alt_L', 'Alt_R', 'Control_L', 'Control_R']
-    if key in names_mod:
-       return codes_mod[names_mod.index(key)]
-    keys_special={' ':'space','\t':'Tab','\n':'Return','\r':'BackSpace','\e':'Escape',
+    keys_special={' ':'space','\t':'Tab','\n':'Return','\b':'BackSpace','\e':'Escape',
                   '!':'exclam','#':'numbersign','%':'percent','$':'dollar','&':'ampersand','"':'quotedbl',
                   '\'':'apostrophe','(':'parenleft',')':'parenright','*':'asterisk','=':'equal','+':'plus',
                   ',':'comma','-':'minus','.':'period','/':'slash',':':'colon',';':'semicolon','<':'less',
@@ -137,18 +235,21 @@ def string_to_keycode(key):
     return disp.keysym_to_keycode(key_sym)
 
 def keycode_to_string(key_code):
-    codes_mod=[50,62,64,108,37,105,113,111,114,116,110,115,112,117,118,119,107,
-               67,68,69,70,71,72,73,74,75,76,95,96
-              ]
+    syms_mod=[65505,65506,65513,65514,65507,65508,
+              65361,65362,65363,65364,65360,65367,65365,65366,
+              65379,65535,65491,
+              65470,65471,65472,65473,65474,65475,65476,65477,65478,65479,65480,65481
+             ]
     names_mod=['Shift_L', 'Shift_R', 'Alt_L', 'Alt_R', 'Control_L', 'Control_R',
                'Left', 'Up', 'Right', 'Down', 'Home', 'End', 'Page_Up', 'Page_Down',
                'Insert', 'Delete', 'Print',
                'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12'
               ]
-    if key_code in codes_mod:
-       return names_mod[codes_mod.index(key_code)]
     #2nd: 0 is unshifted, 1 is shifted, 2 is alt grid, and 3 is shiftalt grid
-    return XK.keysym_to_string(disp.keycode_to_keysym(key_code, 0))
+    key_sym=disp.keycode_to_keysym(key_code, 0)
+    if key_sym in syms_mod:
+       return names_mod[syms_mod.index(key_sym)]
+    return XK.keysym_to_string(key_sym)
 
 def send_event(key, mod, act):
     print 'send key,mod,act:'+str(key)+','+str(mod)+','+str(act)
@@ -164,8 +265,10 @@ def send_event(key, mod, act):
        return
 
     if key in [X.Button1,X.Button2,X.Button3,X.Button4,X.Button5]:
-       wind=root.query_pointer().child
-       window=root.query_pointer().child.query_pointer().child #in some linux, it is root.query_pointer().child
+       wind=root
+       window=root.query_pointer().child
+       #wind=root.query_pointer().child
+       #window=root.query_pointer().child.query_pointer().child #in some linux, it is root.query_pointer().child
        key_action=[X.ButtonPress,X.ButtonRelease][act]
        if act:
           KeyAction=ButtonRelease
@@ -221,19 +324,20 @@ def ungrab_key(key, mod):
     root.ungrab_key(key_code, mod|X.LockMask|X.Mod2Mask)
 
 def handle_event(evt):
-    global is_ready_switchall,key_delay_shift
+    global is_ready_switchall,key_delay_shift,is_on
     key_name=keycode_to_string(evt.detail)
     mod=evt.state & ~(X.LockMask | X.Mod2Mask | 256 | 1024 | 2048) #strip caps lock and num lock, and left mouse pressed
-    print 'receive keycode,keyname,type,state:'+str(evt.detail)+','+key_name+','+str(evt.type)+','+str(mod)
+    print 'receive keycode,keyname,type,state:'+str(evt.detail)+','+key_name+','+str(evt.type)+','+str(mod)+','+('is_on' if is_on else'is_off')
     key={'key':key_name, 'mod':mod}
-    if not key in keys:
+    if (is_on and key in keys) or ((key_name=='Control_L' or key_name=='Control_R') and mod==4):
+        key_mapped=keys_mapped[keys.index(key)]
+    else:
+       print '==================key_name:'+key_name+','+str(mod)
        if evt.type==X.KeyRelease: #only can send key after previous key released
           is_ready_switchall=0
-          key_delay_shift=key_name  #because shift key still pressed
-       return
-    key_mapped=keys_mapped[keys.index(key)]
+          key_delay_shift=key_name  #because Control_* key still pressed
+       key_mapped=key
 
-    global is_on
     if evt.type==X.KeyPress:
        if key_mapped['key']=='SwitchAll':
           is_ready_switchall=1
@@ -257,9 +361,7 @@ def handle_event(evt):
                  send_event(key_delay_shift, X.ShiftMask, 1)
                  key_delay_shift=''
             return
-         if not is_on:#shift is pressed, grabbed something, but off status should grab nothing
-            key_delay_shift=key_name #because shift key still pressed
-            return
+
          if not key_mapped['key'] in ['MoveLeft', 'MoveRight', 'MoveUp', 'MoveDown',
                                       X.Button1,X.Button2,X.Button3,X.Button4,X.Button5
                                      ]:
@@ -267,17 +369,21 @@ def handle_event(evt):
             send_event(key_mapped['key'], key_mapped['mod'], 1)
 
 def switch_all():
-    global is_on
+    global is_on,is_ready_switchall
     for key in keys:
-        if key['key']!='Shift_L' and key['key']!='Shift_R':
+        #print 'key:'+key['key']
+        if key['key']!='Control_L' and key['key']!='Control_R':
            if not is_on:
               grab_key(key['key'], key['mod'])
            else:
                 ungrab_key(key['key'], key['mod'])
     if is_on:
        is_on=0
+       webview.load_html('off<script>document.body.style.backgroundColor="white";</script>')
     else:
+         os.system('xset r rate 200 30')
          is_on=1
+         webview.load_html('on<script>document.body.style.backgroundColor="green";</script>')
     is_ready_switchall=0
 
 from Xlib.ext import record
@@ -291,7 +397,7 @@ def process_replay(replay):
           except:
                  continue
           if evt.type==X.KeyPress:# and (key_name=='Shift_L' or key_name=='Shift_R'):
-             print evt.detail,evt.state
+             print 'replay:',evt.detail,evt.state
              #switch_all()
 
 def listen_shift():
@@ -306,20 +412,94 @@ def listen_shift():
     disp.record_enable_context(ctx, process_replay)
     disp.record_free_context(ctx)
 
+def get_window(title=str):
+    window_ids=root.get_full_property(disp.intern_atom('_NET_CLIENT_LIST'), X.AnyPropertyType).value
+    for window_id in window_ids:
+        wind=disp.create_resource_object('window', window_id)
+        #pid=wind.get_full_property(disp.intern_atom('_NET_WM_PID'), X.AnyPropertyType)
+        if wind.get_wm_name()==title:
+            return wind
+
+def dockize(wind):
+    wind.change_property(disp.intern_atom('_NET_WM_STRUT'),
+                         disp.intern_atom('CARDINAL'),
+                         32,
+                         [0, 0, 0, 70]
+                        )
+
+    #disable minimize:
+    #got the data format from 'xtrace xprop -id 0x02e00007 -f _NET_WM_WINDOW_TYPE 32a -set _NET_WM_WINDOW_TYPE _NET_WM_WINDOW_TYPE_DOCK'
+    wind.change_property(disp.intern_atom('_NET_WM_WINDOW_TYPE'),
+                         disp.intern_atom('ATOM'),
+                         32,
+                         [disp.intern_atom('_NET_WM_WINDOW_TYPE_DOCK')]
+                        )
+
+    #set position:
+    #wind.configure(x=0, y=1012)
+    root.send_event(event=ClientMessage(window=wind,
+                                  client_type=disp.intern_atom('_NET_MOVERESIZE_WINDOW'),
+                                  data=(32, ([1<<8|1<<9, 0, 1012, 0, 0]))
+                                 ),
+                    event_mask=X.SubstructureRedirectMask
+                   )
+
+    #make always above and hide taskbar
+    #got the data format from 'xtrace wmctrl -r "jack" -b add,above,skip_taskbar'
+    root.send_event(event=ClientMessage(window=wind,
+                                  client_type=disp.intern_atom('_NET_WM_STATE'),
+                                  data=(32, ([_NET_WM_STATE_ADD, disp.intern_atom('_NET_WM_STATE_ABOVE'), disp.intern_atom('_NET_WM_STATE_SKIP_TASKBAR'), 0, 0]))
+                                 ),
+                    event_mask=(X.SubstructureRedirectMask)
+                   )
+    #all_states=wind.get_full_property(disp.intern_atom('_NET_WM_STATE'), X.AnyPropertyType)
+    #pprint(all_states)
+
+    #remove title bar:
+    GdkX11.X11Window.foreign_new_for_display(GdkX11.X11Display.get_default(), wind.id).set_decorations(0)
+
+    disp.sync()
+
 def main():
     #listen_shift()
-    #print string_to_keycode('F1')
-    grab_key('Shift_L', X.NONE)
-    grab_key('Shift_R', X.NONE)
+    #print 'hello'+str(string_to_keycode('Shift_L'))
+    grab_key('Control_L', X.ControlMask)
+    grab_key('Control_R', X.ControlMask)
     send_event(X.Button1, X.NONE, 1)
     send_event(X.Button2, X.NONE, 1)
     send_event(X.Button3, X.NONE, 1) #maybe you pressed right mouse button, but myboard.py killed, then mouse and keyboard blocked
     send_event(X.Button4, X.NONE, 1)
     send_event(X.Button5, X.NONE, 1)
+
+    time.sleep(1)
+
+    wind=get_window('myboard_jack')
+    dockize(wind)
+    webview.load_html('off<script>document.body.style.backgroundColor="white";</script>')
+
+    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', 21405))
+    sock.settimeout(0.001);
     while 1:
-          evt=root.display.next_event()
-          if evt.type in [X.KeyPress, X.KeyRelease]: #ignore X.MappingNotify(=34)
-             handle_event(evt)
+          if root.display.pending_events()>0:
+              evt=root.display.next_event()
+              if evt.type in [X.KeyPress, X.KeyRelease]: #ignore X.MappingNotify(=34)
+                 handle_event(evt)
+              #else handle_event_pinyin(evt)
+
+          data=''
+          try:
+              data, address=sock.recvfrom(4096)
+              if data!='':
+                  print 'receive udp:',data
+                  #{"detail":"j", "state":0, "type":2}
+                  #{"detail":"j", "state":0, "type":3}
+                  evt_key=json.loads(data);
+                  evt_key={'detail':string_to_keycode(evt_key['detail']), 'state':evt_key['state'], 'type':evt_key['type']}
+                  handle_event(namedtuple('Struct', evt_key.keys())(*evt_key.values()))
+          except socket.error:
+              continue
 
 if __name__ == '__main__':
-   main()
+    thread.start_new_thread(main, ())
+    webview.create_window('myboard_jack', 'https://www.google.com', 1920, 15, False, False, (1920, 10))
